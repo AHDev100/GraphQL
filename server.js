ac887397b82f68660d05fa3dd8d644f0e9891a3c
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-17 20:10:04 -0400

    Learned how to integrate Apollo Sever with Express

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const app = require('express')(); 
+const express = require('express'); 
 const path = require('path');
-const { graphqlHTTP } = require('express-graphql'); //Express middleware function responding to graphQL queries
+const { ApolloServer } = require('apollo-server-express');
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync('**/*', {
     extensions: ['.graphql'],
 });
-
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray
-});
+async function startApolloServer(){
+    const app = express();
+    
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray
+    });
 
-const root = {
-    products: require('./products/products.model'), 
-    orders: require('./orders/orders.model'),
+    const server = new ApolloServer({
+        schema
+    });
+
+    await server.start();
+    server.applyMiddleware({app, path: '/graphql'});
+
+    app.listen(3000, () => {
+        console.log('Running GraphQL Server...');
+    });
 };
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
-
-app.listen(3000, () => {
-    console.log('Running GraphQL Server...');
-});
+startApolloServer();
